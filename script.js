b2e6bdab72450f6b38af12bf279ac546a3d95713
Emma <cemma313@gmail.com>
2023-02-15 21:15:32 -0700

    js changes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Assignment code here
 var numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var passwordLength
-
+var confirmLower
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,10 +17,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
-  passwordLength = alert("Must be between 8 and 128 characters");
+  passwordLength = prompt("Choose a password length between 8 and 128 characters");
+
 }
