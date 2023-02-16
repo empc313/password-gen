93ed53a466082a8e8814de3357adefdc26b59f13
Emma <cemma313@gmail.com>
2023-02-15 21:41:45 -0700

    js change

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // Assignment code here
-var numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,]
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-var passwordLength
-var confirmLower
+var passwordLength = 128;
+var password = "";
+var confirmLower;
+var confirmUpper;
+var confirmSpecial;
+var confirmNumber;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -20,7 +25,19 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
-  passwordLength = prompt("Choose a password length between 8 and 128 characters");
+var password=document.getElementById("password");
 
+function generatePassword(){
+  passwordLength = prompt ("How many characters woudl you like to use?")
+  
+  if(passwordLength){
+    //code
+  }
+else if (confirmUpper && confirmNumber && confirmSpecial) {
+  userChoices = upperCase.concat(numbers, special);
+  console.log(userChoices);
 }
+}
+
+
+
