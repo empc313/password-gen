d55e0b70d4aa9ffc16fb1beb7ed92a0191cf35df
Emma <cemma313@gmail.com>
2023-02-15 21:58:15 -0700

    password length

--- script.js
+++ script.js
@@ -1,8 +1,94 @@
 // Assignment code here
-var numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "H",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var special = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  " < ",
+  "=",
+  " > ",
+  " ? ",
+  "@",
+  "[",
+  "\\",
+  "]",
+  " ^ ",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
 var passwordLength;
 var password = "";
 var confirmLower;
@@ -13,32 +99,26 @@ var confirmNumber;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var password=document.getElementById("password");
+var password = document.getElementById("password");
 
-function generatePassword(){
-  passwordLength = prompt ("How many characters would you like to use?")
-  
-  if(passwordLength<7){
+function generatePassword() {
+  passwordLength = prompt("How many characters would you like to use?");
+
+  if (passwordLength < 7) {
     alert("Password must be between 8 and 128 characters");
-    determineLength();}
-    else if (passwordLength>129){
-      alert("Password must be between 8 and 128 characters");
-    }
-    return passwordLength;
+    determineLength();
+  } else if (passwordLength > 129) {
+    alert("Password must be between 8 and 128 characters");
   }
-
-
-
-
+  return passwordLength;
+}
