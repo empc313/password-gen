9d2cdc58b7daa375b80b748289aaa6a2b60bbb50
Emma <cemma313@gmail.com>
2023-02-16 16:39:55 -0700

    rearragned code

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Assignment code here
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var passwordLength;
+var password = "";
+var confirmLower;
+var confirmUpper;
+var confirmSpecial;
+var confirmNumber;
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  
+passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var password = document.getElementById("password");
+//function to determine the length of the pssword
+function generatePassword() {
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowerCase = [
   "a",
   "b",
@@ -89,36 +113,18 @@ var special = [
   "}",
   "~",
 ];
-var passwordLength;
-var password = "";
-var confirmLower;
-var confirmUpper;
-var confirmSpecial;
-var confirmNumber;
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-var password = document.getElementById("password");
-
-function generatePassword() {
+var password = ""
   passwordLength = prompt("How many characters would you like to use?");
-
-  if (passwordLength < 7) {
-    alert("Password must be between 8 and 128 characters");
-    determineLength();
-  } else if (passwordLength > 129) {
-    alert("Password must be between 8 and 128 characters");
+for ( var i=0; i <=passwordLength; i++);
+var randomNumber = Math.floor(Math.random()* chars.length)
+document.getElementById("password").value = password;
+//function used to choose special characters
+function determineSpecial(){
+confirmSpecial = prompt ("Would you like to use special characters?");
+//defining string varibles
+if (confirmSpecial === null || confirmSpecial === ""){
+  alert("yes or no");
+  determineSpecial();
+} else if (confirmSpecial === "yes" || confirmSpecial ==="y");
+};
   }
-  return passwordLength;
-}
