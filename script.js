3d6693bc9e872451da4dea751c6f5bc8dd2e0b22
Emma <cemma313@gmail.com>
2023-03-02 13:39:16 -0700

    cleaned js and worked on password generation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Variables
-  var lowerCase =  [
+var charBank = "";
+var lowerCase = [
     "a",
     "b",
     "c",
@@ -88,48 +89,55 @@ var special = [
     "}",
     "~",
   ];
-  
-
 
 //confirm if password should include the following
-// var chars = [];
 var includeNumbers = confirm("Would you like to use numbers?");
 var includeLower = confirm("Would you like to use lower-case letter?");
 var includeUpper = confirm("Would you like to use upper-case letters?");
 var includeSpecial = confirm("Would you like to include special charcters?");
 
 
-//function to generate the type of password you want to create
-function generatePassword(){
-var charBank = [];
-if(includeNumbers){
-  numbers = charBank.concat(charBank.numbers);
-}
-if(includeLower){
-  lowerCase = charBank.concat(charBank.lowerCase);
-}
-if(includeUpper){
-  upperCase = charBank.concat(charBank.upperCase);
-}
-if(includeSpecial){
-  speechSynthesis = charBank.concat(charBank.special);
-}
-  var passwordLength = prompt("Please choose a password length between 8 and 128 charcters");
 
-  if (passwordLength<7 || passwordLength>129){
-  alert("Please choose a password length between 8 and 128 charcters");
-  return "Invalid";
-}}
-  
 function writePassword(){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-  //generate password on click
-  var generateBtn =document.querySelector('#generate');
+
+//function to generate the type of password you want to create
+function generatePassword(){
+  if(includeNumbers){
+    includeNumbers = charBank.concat(charBank.numbers);
+  }
+  if(includeLower){
+    includeLower = charBank.concat(charBank.lowerCase);
+  }
+  if(includeUpper){
+    upperCase = charBank.concat(charBank.upperCase);
+  }
+  if(includeSpecial){
+    speechSynthesis = charBank.concat(charBank.special);
+  }
+  var passwordLength = prompt("Please choose a password length between 8 and 128 charcters");
+  
+  if (passwordLength<7 || passwordLength>129){
+    alert("Please choose a password length between 8 and 128 charcters");
+    return "Invalid";
+  }}
+  
+  // function generatePassword(){
+  //   var generate = "";
+  //   for(i = 0; password; i++){
+  //     var index = Math.floor(Math.random()* charBank.length);
+  //     var randomPass = charBank[index];
+  //     generate += randomPass;
+  //   }}
+    
+    
+    //generate password on click
+var generateBtn =document.querySelector('#generate');
   generateBtn.addEventListener("click", writePassword);
  
-console.log(writePassword)
 
+console.log(generate)
 
