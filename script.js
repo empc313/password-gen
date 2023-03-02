5d45b46f32733196167adfd4b4d22b6a21c8cbab
Emma <cemma313@gmail.com>
2023-03-02 12:37:02 -0700

    prompt for questions to generate password based on perimeters

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //Variables
-function generatePassword(){
-  var passwordLength = prompt("Please choose a password length between 8 and 128 charcters");
-  var lowerCase = prompt("Would you like to use numbers?"); [
+  var lowerCase =  [
     "a",
     "b",
     "c",
@@ -90,13 +88,7 @@ var special = [
     "}",
     "~",
   ];
-  var numbers =['0','1','2','3','4','5','6','7','8','9']
-  if (passwordLength<7 || passwordLength>129){
-  alert("Please choose a password length between 8 and 128 charcters");
-  return "Invalid";
-}}
-
-//declaring function to generate password
+  
 
 
 //confirm if password should include the following
@@ -106,6 +98,9 @@ var includeLower = confirm("Would you like to use lower-case letter?");
 var includeUpper = confirm("Would you like to use upper-case letters?");
 var includeSpecial = confirm("Would you like to include special charcters?");
 
+
+//function to generate the type of password you want to create
+function generatePassword(){
 var charBank = [];
 if(includeNumbers){
   numbers = charBank.concat(charBank.numbers);
@@ -119,19 +114,12 @@ if(includeUpper){
 if(includeSpecial){
   speechSynthesis = charBank.concat(charBank.special);
 }
-function generatePassword(){
   var passwordLength = prompt("Please choose a password length between 8 and 128 charcters");
 
   if (passwordLength<7 || passwordLength>129){
   alert("Please choose a password length between 8 and 128 charcters");
   return "Invalid";
 }}
-// //make a random varible to genrate password
-// var pass ="";
-//  for (i = 0; i < passwordLength; i++){
-//   var randomNumber = Math.floor(Math.random()*chars.length);
-//   var randomChar = char[randomNumber];
-//   pass += randomChar;}
   
 function writePassword(){
   var password = generatePassword();
@@ -142,44 +130,6 @@ function writePassword(){
   var generateBtn =document.querySelector('#generate');
   generateBtn.addEventListener("click", writePassword);
  
+console.log(writePassword)
 
 
-
-// var charBank = []
-// if(includeLower){
-//   return(charBank.lowerCase)
-// }
-// console.log(charBank)
-
-
-
-//   for (var i = 0; i < passwordLength; i++);
-//   //generate a number
-//   var randomNumber = Math.floor(Math.random() * charBank.length)
-// var newChar =charBank[randomNumber]
-// password = password + newChar
-//   //function used to choose special characters
-
-
-// // Assignment code here
-// var passwordLength;
-// var password = "";
-// var confirmLower;
-// var confirmUpper;
-// var confirmSpecial;
-// var confirmNumber;
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// var password = document.getElementById("password");
