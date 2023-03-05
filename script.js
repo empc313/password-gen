0761783ace210b6262ef45377ed80539203895c9
Emma <cemma313@gmail.com>
2023-03-05 09:26:31 -0700

    updated js

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
 //Variables
 var charBank = "";
 var lowerCase = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  var upperCase = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "H",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "H",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 var special = [
-    "!",
-    "#",
-    "$",
-    "%",
-    "&",
-    "'",
-    "(",
-    ")",
-    "*",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    ":",
-    ";",
-    " < ",
-    "=",
-    " > ",
-    " ? ",
-    "@",
-    "[",
-    "\\",
-    "]",
-    " ^ ",
-    "_",
-    "`",
-    "{",
-    "|",
-    "}",
-    "~",
-  ];
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  " < ",
+  "=",
+  " > ",
+  " ? ",
+  "@",
+  "[",
+  "\\",
+  "]",
+  " ^ ",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
 
 //confirm if password should include the following
 var includeNumbers = confirm("Would you like to use numbers?");
@@ -96,48 +96,46 @@ var includeLower = confirm("Would you like to use lower-case letter?");
 var includeUpper = confirm("Would you like to use upper-case letters?");
 var includeSpecial = confirm("Would you like to include special charcters?");
 
-
-
-function writePassword(){
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 //function to generate the type of password you want to create
-function generatePassword(){
-  if(includeNumbers){
+function generatePassword() {
+  if (includeNumbers) {
     includeNumbers = charBank.concat(charBank.numbers);
   }
-  if(includeLower){
+  if (includeLower) {
     includeLower = charBank.concat(charBank.lowerCase);
   }
-  if(includeUpper){
+  if (includeUpper) {
     upperCase = charBank.concat(charBank.upperCase);
   }
-  if(includeSpecial){
+  if (includeSpecial) {
     speechSynthesis = charBank.concat(charBank.special);
   }
-  var passwordLength = prompt("Please choose a password length between 8 and 128 charcters");
-  
-  if (passwordLength<7 || passwordLength>129){
+  var passwordLength = prompt(
+    "Please choose a password length between 8 and 128 charcters"
+  );
+
+  if (passwordLength < 7 || passwordLength > 129) {
     alert("Please choose a password length between 8 and 128 charcters");
     return "Invalid";
-  }}
-  
-  // function generatePassword(){
-  //   var generate = "";
-  //   for(i = 0; password; i++){
-  //     var index = Math.floor(Math.random()* charBank.length);
-  //     var randomPass = charBank[index];
-  //     generate += randomPass;
-  //   }}
-    
-    
-    //generate password on click
-var generateBtn =document.querySelector('#generate');
-  generateBtn.addEventListener("click", writePassword);
- 
+  }
+}
 
-console.log(generate)
+// function generatePassword(){
+//   var generate = "";
+//   for(i = 0; password; i++){
+//     var index = Math.floor(Math.random()* charBank.length);
+//     var randomPass = charBank[index];
+//     generate += randomPass;
+//   }}
 
+//generate password on click
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+
+console.log(generate);
