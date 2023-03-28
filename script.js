814c5b61e4ffad94ead7f2587245381d95b8e4ad
Emma <cemma313@gmail.com>
2023-03-28 16:11:43 -0600

    password generated

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Variables
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowerCase = [
@@ -90,44 +89,47 @@ var special = [
   "}",
   "~",
 ];
-var newPassword =[];
-var charBank = number.concat(lowerCase).concat(upperCase).concat(special);
-
+var charBank = [];
 
 //function to generate the type of password you want to create
-function generatePassword() { 
+function generatePassword() {
+  var password = "";
   var includeNumbers = confirm("Would you like to use numbers?");
-  if (includeNumbers === true) {
-    
-  }
   var includeLower = confirm("Would you like to use lower-case letter?");
-  if (includeLower === true) {
-    
-  }
   var includeUpper = confirm("Would you like to use upper-case letters?");
-  if (includeUpper === true) {
-    true;
-  }
   var includeSpecial = confirm("Would you like to include special charcters?");
-  if (includeSpecial) {
-    true;
-  }
   var passwordLength = prompt(
     "Please choose a password length between 8 and 128 charcters"
-    );
-    if (passwordLength < 7 || passwordLength > 129) {
-      alert("Please choose a password length between 8 and 128 charcters");
-      return "Invalid";
-    } 
-     
-    return  charBank[Math.floor(Math.random() * charBank.length)];
+  );
+  passwordLength = parseInt(passwordLength);
+  if (includeNumbers === true) {
+    charBank = charBank.concat(number);
   }
-  
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+  if (includeLower === true) {
+    charBank = charBank.concat(lowerCase);
   }
+  if (includeUpper === true) {
+    charBank = charBank.concat(upperCase);
+  }
+  if (includeSpecial === true) {
+    charBank = charBank.concat(special);
+  }
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a password length between 8 and 128 charcters");
+    return "Invalid";
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    password += charBank[Math.floor(Math.random() * charBank.length)];
+  }
+  return password;
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 //generate password on click
 var generateBtn = document.querySelector("#generate");
